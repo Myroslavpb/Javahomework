36958a817f6d0a4310d9100f5412c38a992a79cd
Myroslavpb <johndoe@example.com>
2021-10-15 23:14:30 +0300

    init commit

--- src/com/pb/Nikitiuk/HW2/Calculator.java
+++ src/com/pb/Nikitiuk/HW2/Calculator.java
@@ -3,8 +3,42 @@ package com.pb.Nikitiuk.HW2;
 import java.util.Scanner;
 
 public class Calculator { public static void main(String[] args ) {
+  String sign;
   int y, x;
-  x =1;
- System.out.println("x=" +x);
-    }
+
+  Scanner scan = new Scanner(System.in);
+    System.out.println("Введіть число і натисніть enter ");
+  x= scan.nextInt();
+  System.out.println("Введіть операцію яку бажаєте виконати : +; -; *; /");
+  sign = scan.next();
+
+    System.out.println("Введіть число і натисніть enter ");
+    y= scan.nextInt();
+
+
+switch (sign) {
+  case "+" :
+      System.out.println(" =" + (x +y));
+         break;
+  case "-":
+    System.out.println("=" + (x-y));
+     break;
+  case "*":
+    System.out.println("=" + (x*y));
+    break;
+  case "/":
+    if (y==0)
+      System.out.println("Universe has been destroyed downloading  last save");
+    else
+    System.out.println("=" + (x/y));
+
+
+    break;
 }
+  }
+
+
+
+  }
+
+
--- src/com/pb/Nikitiuk/HW2/Interval.java
+++ src/com/pb/Nikitiuk/HW2/Interval.java
@@ -1,6 +1,47 @@
 package com.pb.Nikitiuk.HW2;
 
+import java.util.Scanner;
+
 public class Interval {
+    public static void main(String[] args ){
+        Scanner scan = new Scanner(System.in);
     int x;
 
-}
+
+System.out.println("Введіть ціле число від 0 до 100");
+
+x=scan.nextInt();
+    if(x >= 0 & x < 101)
+    {
+         if ( x >= 0 & x <= 14  )
+                System.out.println("Ваше число належить інтервалу від 0 до 14");
+            if (x <= 35 & x > 14)
+                System.out.println("Ваше число належить інтервалу від 14 до 35");
+            if (x <= 50 & x > 35)
+                System.out.println("Ваше число належить інтервалу від 36 до 50");
+            if (x <= 100 && x > 50)
+                System.out.println("Ваше число належить інтервалу від 51 до 100");
+        }
+
+    else System.out.println("Спробуй ще раз");
+
+
+
+                                 ;
+
+
+
+
+         }
+
+
+
+
+
+
+
+
+
+
+    }
+
