1a72b4a3977134acdd84433a5416bd5b697b635c
Myroslavpb <johndoe@example.com>
2021-10-24 23:44:15 +0300

    init commit

--- src/com/pb/Nikitiuk/HW3/Array.java
+++ src/com/pb/Nikitiuk/HW3/Array.java
@@ -4,35 +4,31 @@ import java.util.Scanner;
 
 public class Array {
 
-        public static void main(String[] args ){
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Print 10 numbers");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int array[] = new int[10];
+        System.out.println("Insert array elements:");
+        for (int i = 0; i < 10; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.print("Inserted array elements:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(" " + array[i]); // Выводим на экран, полученный массив
+        }
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = sum + array[i];
+        }
+        System.out.println();
+        System.out.println(" sum = " + sum);
 
-int array[] = new int[10];
-            array [0] = scan.nextInt();
-            array [1] = scan.nextInt();
-            array [2] = scan.nextInt();
-            array [3] = scan.nextInt();
-            array [4] = scan.nextInt();
-            array [5] = scan.nextInt();
-            array [6] = scan.nextInt();
-            array [7] = scan.nextInt();
-            array [8] = scan.nextInt();
-            array [9] = scan.nextInt();
+        int positive = 0;
+        for (int i = 0; i < 10; i++){
+            if (array[i] > 0 )
+            positive ++;}
+            System.out.println("Positive elements: " + positive);
+        System.out.println();
 
-            System.out.println(array[0] +  array[1] +  array[2] + array[3] + array[4] + array[5] + array[3]);
-            System.out.println(array[1]);
-            System.out.println(array[2]);
-            System.out.println(array[3]);
-            System.out.println(array[4]);
-            System.out.println(array[5]);
-            System.out.println(array[6]);
-            System.out.println(array[7]);
-            System.out.println(array[8]);
-            System.out.println(array[9]);
-            System.out.println("Sum of ellements = " );
 
         }
-
-
-}
+    }
