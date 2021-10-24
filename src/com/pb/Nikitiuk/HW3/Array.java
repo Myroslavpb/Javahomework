505556fbcb9bb17a9c769b4ff54aa8777da89d47
Myroslavpb <johndoe@example.com>
2021-10-24 22:55:37 +0300

    init commit

--- src/com/pb/Nikitiuk/HW3/Array.java
+++ src/com/pb/Nikitiuk/HW3/Array.java
@@ -1,9 +1,37 @@
 package com.pb.Nikitiuk.HW3;
 
+import java.util.Scanner;
+
 public class Array {
 
         public static void main(String[] args ){
-            System.out.println("HelloWorld");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Print 10 numbers");
+
+int array[] = new int[10];
+            array [0] = scan.nextInt();
+            array [1] = scan.nextInt();
+            array [2] = scan.nextInt();
+            array [3] = scan.nextInt();
+            array [4] = scan.nextInt();
+            array [5] = scan.nextInt();
+            array [6] = scan.nextInt();
+            array [7] = scan.nextInt();
+            array [8] = scan.nextInt();
+            array [9] = scan.nextInt();
+
+            System.out.println(array[0] +  array[1] +  array[2] + array[3] + array[4] + array[5] + array[3]);
+            System.out.println(array[1]);
+            System.out.println(array[2]);
+            System.out.println(array[3]);
+            System.out.println(array[4]);
+            System.out.println(array[5]);
+            System.out.println(array[6]);
+            System.out.println(array[7]);
+            System.out.println(array[8]);
+            System.out.println(array[9]);
+            System.out.println("Sum of ellements = " );
+
         }
 
 
